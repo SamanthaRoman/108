a7627a066c3cec5187aa5f82ecc403a4f07fc8b4
Samantha Roman <samantharoman@Samanthas-MBP-2.lan>
2023-10-14 15:07:55 -0400

    assignment 1

---
+++ test.py
@@ -0,0 +1,39 @@
+print("Hello, world!")
+# in JS we say let temperature = value
+# python create a variable like this:
+temperature = 98.8
+# strings
+name = "adrian"
+# numbers
+number = 123
+# bulyon 
+bol = True
+print(bol)
+
+
+# addition
+result = 10 + 5
+# subtraction 
+result = 20 - 7
+# multiplication
+result = 6 * 4
+
+x = 10
+
+# JS for loop for(i=0;x<y;i++)
+# for loop for PY
+for i in range(5):
+    print(i)
+
+# arrays in py are called list
+
+fruits = ["apple","bannana","cherry"]
+print([fruits[0]])
+
+
+# function nameOf()
+# print is like the console log
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Adrian")
